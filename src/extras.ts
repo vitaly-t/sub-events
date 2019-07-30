49c03bb6582de7c70761bfd4024863eac69f4d7d
Vitaly Tomilov <vitaly.tomilov@gmail.com>
2019-07-30 13:51:47 +0100

    exporting the extras namespace

--- src/extras.ts
+++ src/extras.ts
@@ -13,58 +13,62 @@
 
 import {SubEvent, ISubContext, SubEventCount} from './';
 
-/**
- * Example of one-to-one event wrapping for global type Event.
- *
- * In this context, one-to-one means that every `subscribe` call results
- * in the immediate `addEventListener` call, and every `cancel` call on
- * the returned `Subscription` object results in the immediate call of
- * `removeEventListener`.
- *
- * This implementation is simplistic, just to show the general approach:
- *  - it assumes that in a browser, you would want to forward events
- *    synchronously, so it uses `emitSync`;
- *  - it does not use `emitSyncSafe` to let you accumulate errors;
- *
- *  But you can easily extend it to allow for much more flexibility.
- */
-export function fromEvent(source: Node, event: string): SubEvent<Event> {
-    const onSubscribe = (ctx: ISubContext<Event, EventListener>) => {
-        ctx.data = e => ctx.event.emitSync(e); // our event handler
-        source.addEventListener(event, ctx.data, false);
-    };
-    const onCancel = (ctx: ISubContext<Event, EventListener>) => {
-        source.removeEventListener(event, ctx.data, false);
-    };
-    return new SubEvent<Event>({onSubscribe, onCancel});
-}
+// istanbul ignore file;
+export namespace extras {
 
-/**
- * Example of wrapping global Event-s in a shared manner, based on their subscription count:
- * - we call `addEventListener` whenever the first subscriber has been registered;
- * - we call `removeEventListener` after the last subscription has been cancelled.
- *
- * Such approach is suitable primarily for globally used events, because we need to permanently
- * maintain our subscription for event `onCount`, and so we never cancel it.
- *
- * As above, it is a simplified implementation, but you can improve it easily, if needed:
- *  - it assumes that in a browser, you would want to forward events synchronously,
- *    so it uses `emitSync`;
- *  - it does not use `emitSyncSafe` to let you accumulate errors;
- */
-export function fromSharedEvent(source: Node, event: string): SubEventCount<Event> {
-    const sub: SubEventCount<Event> = new SubEventCount();
-    const handler = (e: Event) => sub.emitSync(e);
-    sub.onCount.subscribe(info => {
-        const start = info.prevCount === 0; // fresh start
-        const stop = info.newCount === 0; // no subscriptions left
-        if (start) {
-            source.addEventListener(event, handler, false);
-        } else {
-            if (stop) {
-                source.removeEventListener(event, handler, false);
+    /**
+     * Example of one-to-one event wrapping for global type Event.
+     *
+     * In this context, one-to-one means that every `subscribe` call results
+     * in the immediate `addEventListener` call, and every `cancel` call on
+     * the returned `Subscription` object results in the immediate call of
+     * `removeEventListener`.
+     *
+     * This implementation is simplistic, just to show the general approach:
+     *  - it assumes that in a browser, you would want to forward events
+     *    synchronously, so it uses `emitSync`;
+     *  - it does not use `emitSyncSafe` to let you accumulate errors;
+     *
+     *  But you can easily extend it to allow for much more flexibility.
+     */
+    export function fromEvent(source: Node, event: string): SubEvent<Event> {
+        const onSubscribe = (ctx: ISubContext<Event, EventListener>) => {
+            ctx.data = e => ctx.event.emitSync(e); // our event handler
+            source.addEventListener(event, ctx.data, false);
+        };
+        const onCancel = (ctx: ISubContext<Event, EventListener>) => {
+            source.removeEventListener(event, ctx.data, false);
+        };
+        return new SubEvent<Event>({onSubscribe, onCancel});
+    }
+
+    /**
+     * Example of wrapping global Event-s in a shared manner, based on their subscription count:
+     * - we call `addEventListener` whenever the first subscriber has been registered;
+     * - we call `removeEventListener` after the last subscription has been cancelled.
+     *
+     * Such approach is suitable primarily for globally used events, because we need to permanently
+     * maintain our subscription for event `onCount`, and so we never cancel it.
+     *
+     * As above, it is a simplified implementation, but you can improve it easily, if needed:
+     *  - it assumes that in a browser, you would want to forward events synchronously,
+     *    so it uses `emitSync`;
+     *  - it does not use `emitSyncSafe` to let you accumulate errors;
+     */
+    export function fromSharedEvent(source: Node, event: string): SubEventCount<Event> {
+        const sub: SubEventCount<Event> = new SubEventCount();
+        const handler = (e: Event) => sub.emitSync(e);
+        sub.onCount.subscribe(info => {
+            const start = info.prevCount === 0; // fresh start
+            const stop = info.newCount === 0; // no subscriptions left
+            if (start) {
+                source.addEventListener(event, handler, false);
+            } else {
+                if (stop) {
+                    source.removeEventListener(event, handler, false);
+                }
             }
-        }
-    });
-    return sub;
+        });
+        return sub;
+    }
 }
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
 export {Subscription} from './sub';
 export {SubEvent, ISubContext, IEventOptions} from './event';
 export {SubEventCount, ICountOptions, ISubCountChange} from './count';
+export {extras} from './extras';
