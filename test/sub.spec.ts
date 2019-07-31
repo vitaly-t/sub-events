327addc512df509bcd63b215b08819086bec02d0
Vitaly Tomilov <vitaly.tomilov@gmail.com>
2019-07-31 09:24:22 +0100

    adding tests

---
+++ test/sub.spec.ts
@@ -0,0 +1,22 @@
+import {expect, chai} from './';
+import {SubEvent} from '../src';
+
+describe('Subscription', () => {
+    it('must maintain the live status correctly', () => {
+        const a = new SubEvent();
+        const sub = a.subscribe(() => {
+        });
+        expect(sub.live).to.be.true;
+        sub.cancel();
+        expect(sub.live).to.be.false;
+    });
+    it('must do nothing on repeated cancel', () => {
+        const a = new SubEvent();
+        const sub = a.subscribe(() => {
+        });
+        const res1 = sub.cancel();
+        const res2 = sub.cancel();
+        expect(res1).to.be.true;
+        expect(res2).to.be.false;
+    });
+});
