4571998fd15907465cf15f0edf687c6ba9b929d1
Vitaly Tomilov <vitaly.tomilov@gmail.com>
2019-09-25 07:52:44 +0100

    refactoring tsconfig

---
+++ examples/index.ts
@@ -0,0 +1,4 @@
+export {fromEvent, fromSharedEvent} from './from-event';
+export {fromEmitter, fromSharedEmitter} from './from-emitter';
+export {fromInterval, fromSharedInterval} from './from-interval';
+export {TimeoutEvent, fromTimeout} from './from-timeout';
