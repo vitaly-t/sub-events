7a81251c726423f7e5e733b0a2850ea194310caf
Vitaly Tomilov <vitaly.tomilov@gmail.com>
2019-09-25 22:29:52 +0100

    deployment update

--- examples/deploy.ts
+++ examples/deploy.ts
@@ -5,7 +5,11 @@
 import fs from 'fs';
 import path from 'path';
 
-const srcFiles = ['index.ts', 'from-emitter.ts', 'from-event.ts', 'from-interval.ts', 'from-timeout.ts'];
+const files = {
+    source: ['from-emitter.ts', 'from-event.ts', 'from-interval.ts', 'from-timeout.ts'],
+    aux: ['index.ts', 'tsconfig.json']
+};
+
 const destFolder = '../ext';
 
 const srcInclude = /\.\.\/src/g;
@@ -16,7 +20,15 @@ if (!fs.existsSync(destFolder)) {
     fs.mkdirSync(destFolder);
 }
 
-srcFiles.forEach(f => {
-    const text = fs.readFileSync(f).toString().replace(srcInclude, destInclude);
-    fs.writeFileSync(path.join(__dirname, destFolder, f), text);
-});
+function copyFiles(files: string[], modifyText?: (txt: string) => string) {
+    files.forEach(f => {
+        let text = fs.readFileSync(f).toString();
+        if (modifyText) {
+            text = modifyText(text);
+        }
+        fs.writeFileSync(path.join(__dirname, destFolder, f), text);
+    });
+}
+
+copyFiles(files.source, text => text.replace(srcInclude, destInclude));
+copyFiles(files.aux);
