5d43b22ffc536e4c3bff21a457ccfaeb4844b614
Vitaly Tomilov <vitaly.tomilov@gmail.com>
2019-09-26 00:00:05 +0100

    improving deployment for #17

--- extras/deploy.ts
+++ extras/deploy.ts
@@ -2,11 +2,11 @@
 * Manually deploys all the extras.
 */
 
-import fs from 'fs';
-import path from 'path';
+import * as fs from 'fs';
+import * as path from 'path';
 
-const srcFolder = path.join(__dirname, 'src');
-const destFolder = '../ext';
+const srcFolder = path.join(__dirname, '../deploy/extras/src');
+const destFolder = path.join(__dirname, '../ext');
 
 // Create destination folder, if it doesn't exist:
 if (!fs.existsSync(destFolder)) {
@@ -16,6 +16,6 @@ if (!fs.existsSync(destFolder)) {
 fs.readdirSync(srcFolder).forEach(f => {
     if (f.endsWith('.d.ts') || f.endsWith('.js')) {
         const text = fs.readFileSync(path.join(srcFolder, f)).toString().replace(/\.\.\/\.\.\/src/g, '\.\.\/dist\/src');
-        fs.writeFileSync(path.join(__dirname, destFolder, f), text);
+        fs.writeFileSync(path.join(destFolder, f), text);
     }
 });
