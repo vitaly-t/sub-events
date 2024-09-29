755751c87f5c9724f484c4bb1a4459390ff828f1
Vitaly Tomilov <vitaly-t@users.noreply.github.com>
2024-09-29 16:45:07 +0100

    deps + compile

--- test/index.ts
+++ test/index.ts
@@ -1,8 +1,8 @@
-import chai from 'chai';
+import ch from 'chai';
 import spies from 'chai-spies';
 import {describe} from 'mocha';
 
-chai.use(spies);
+const chai = ch.use(spies);
 
 const expect = chai.expect;
 const should = chai.should();
